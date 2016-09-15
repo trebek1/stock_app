207a12f7b250774c9f1770c932441c8cb93d3737
trebek1 <trebek08@gmail.com>
2016-09-15 15:29:12 -0700

    react boilerplate

---
+++ app/app.jsx
@@ -0,0 +1,9 @@
+// a container component keeps track of state and renders child components 
+// a presentational component uses props to display information 
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+	<h1> Boilerplate </h1>, document.getElementById('app')
+);
